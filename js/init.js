7535b25b7469a6c1a118d0219370c90e8061c6d7
zithron <abdulbasithb70@gmail.com>
2017-07-09 20:59:04 +0700

    tambah dropin

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('[data-toggle="popover"]').popover({
         html: true,
         placement: 'top',
         trigger: 'focus',
-        content: function() {
+        content: function () {
             return '<p>' + $(this).data('deskripsi') + '</p>' +
                 '<p><a href="' + $(this).data('link') + '" target="_blank">' + $(this).data('link') + '</a></p>' +
                 '<img src="' + $(this).data('img') + '" />'
@@ -14,5 +14,14 @@ $(document).ready(function() {
             '<h3 class="popover-title"></h3>' +
             '<div class="popover-content"></div>' +
             '</div>'
+    });
+
+
+    $('#sepeda-kampus').click(function () {
+        $('img.sepeda-kampus').toggle();
+    });
+    
+     $('#kantong-parkir').click(function () {
+        $('img.kantong-parkir').toggle();
     });
 });
